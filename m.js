790d0ef89c21b347e184cf2f5d2570f4988d73f5
zibvh <bhuszibah@gmail.com>
2026-07-21 14:11:04 +0100

    first commit

---
+++ m.js
@@ -0,0 +1,122 @@
+const item = document.getElementById("item")
+const addButton = document.getElementById("submit")
+const todolistparent = document.querySelector(".todolist")
+let listArray = JSON.parse(localStorage.getItem("task")) || []
+const alldone = document.querySelector(".finishall")
+
+
+addButton.addEventListener("click", () => {
+   if(item.value != ""){
+     writeItem()
+    appendItem()
+   }
+   else if(item.value ==''){
+    let alertdiv = document.querySelector(".alertdiv")
+    let alerttext = document.querySelector(".alerttext")
+    let alertcon = document.querySelector(".alertcon")
+    let x = document.querySelector(".x")
+     alertdiv.style.display = "flex"
+     alertcon.style.display = "flex"
+     alerttext.textContent = "Enter A List"
+    alertcon.addEventListener("click", (e) =>{
+        if(e.target === alertcon){
+            alertdiv.style.display = "none"
+        alertcon.style.display = "none"
+        }
+        else if(e.target === alertdiv){
+            return
+        }
+    })
+     x.addEventListener("click", () =>{
+        alertdiv.style.display = "none"
+        alertcon.style.display = "none"
+    })
+   }
+})
+// let alertbox = () => {
+// alertdiv.addEventListener("click", () =>{
+//     alertdiv.style.display = "flex"
+// })
+// }
+
+
+let writeItem = () => {
+    let listObject = {
+        text: item.value,
+        completed: false
+    }
+
+    listArray.unshift(listObject)
+    localStorage.setItem("task", JSON.stringify(listArray))
+}
+
+
+
+let appendItem = () => {
+todolistparent.innerHTML = ""
+    for(let i = 0; i< listArray.length; i++){
+        document.querySelector("p").style.display = "none"
+        let todoListItem = document.createElement("li")
+        let todoListItemSpan = document.createElement("span")
+        todoListItem.append(todoListItemSpan)
+        todoListItemSpan.textContent = listArray[i].text
+        todoListItem.style = `
+            display: flex;
+    justify-content: space-between;
+    align-items: center;
+    `
+            if(listArray[i].completed){
+            todoListItem.style =`
+                    opacity: 0.5;
+    text-decoration: line-through;
+    padding: 3px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+                `
+        }
+
+
+    let iconCon = document.createElement("div")
+    iconCon.innerHTML += `<i class="fa-solid fa-pen-to-square"></i>`
+    iconCon.innerHTML += `<i class="fa-solid fa-trash"></i>`
+    iconCon.innerHTML += `<i class="fa-solid fa-check"></i>`
+        iconCon.style = `
+        display: flex;
+        gap: 8px;
+        
+        `
+        todoListItem.append(iconCon)
+        todolistparent.append(todoListItem)
+        iconCon.addEventListener("click", (e) => {
+
+//finish all logic
+            if(e.target.className === "fa-solid fa-arrow-up-from-bracket"){
+                listArray.completed = !listArray[i].completed
+                localStorage.setItem("task", JSON.stringify(listArray))
+                appendItem()
+            }
+            //complete logic
+            if(e.target.className === "fa-solid fa-check"){
+                listArray[i].completed = !listArray[i].completed
+                localStorage.setItem("task", JSON.stringify(listArray))
+                appendItem()
+            }
+                //delete logic
+            if (e.target.className === "fa-solid fa-trash"){
+            listArray.splice(i, 1)
+            localStorage.setItem("task", JSON.stringify(listArray))
+        appendItem()
+    }
+
+})
+//end of delete logic
+    }
+    item.value = ""
+
+}
+
+
+
+appendItem()
+
